e1f851399a0b61a0b319d873e22dfdcb61b7b7d7
AnshumaN <anshuman9147@gmail.com>
2025-07-21 02:49:25 +0530

    Add subtraction.java and update input/output files
    Added a new Java program for subtraction. Updated input.txt and output.txt to reflect new input values and output for the subtraction operation.

---
+++ subtraction.java
@@ -0,0 +1,10 @@
+
+public class subtraction {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int result = a - b;
+        System.out.println("The result of subtraction is: " + result);
+    }
+    
+}
